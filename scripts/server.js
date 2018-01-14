0ef620bee938bc87dfb3902bc9f403c8b9bf55d5
Karolis Jocevičius <karolis.jocevicius@gmail.com>
2018-01-14 22:11:38 +0200

    Server startup message

--- scripts/server.js
+++ scripts/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+
+var dirToServe = '../dist';
+var port = process.env.PORT || 8080;
+
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '../dist'));
+app.use(dirToServe);
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port);
+
+console.log(`Serving ${dirToServe} on port ${port}`);
